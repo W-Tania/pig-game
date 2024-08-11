3b5731c8e0a74b5078624a2f4e253608dd2de04d
unknown <tania.y.wang@gmail.com>
2024-08-11 21:21:45 +1000

    Generate and Display Dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialState = {
+  scorePlayer1: 0,
+  scorePlayer2: 0,
+  currentPlayer: 0,
+  currentScore: 0,
+  dice: 0,
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function reducer(state, action) {
+  switch (action.type) {
+    case "reset":
+      return initialState;
+    case "rollDice":
+      return {
+        ...state,
+        dice: Math.trunc(Math.random() * 6) + 1,
+        currentScore: state.currentScore + state.dice,
+      };
+    case "hold":
+      return {
+        ...state,
+        scorePlayer1: state.scorePlayer1 + state.currentScore,
+        currentScore: 0,
+      };
+  }
 }
 
-export default App
+function App() {
+  // const [dice, setDice] = useState(0);
+  // function RollDice() {
+  //   setDice(Math.trunc(Math.random() * 6) + 1);
+  // }
+
+  const [
+    { scorePlayer1, scorePlayer2, currentPlayer, currentScore, dice },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  const reset = function () {
+    dispatch({ type: "reset" });
+  };
+
+  const rollDice = function () {
+    dispatch({ type: "rollDice" });
+    console.log(dice);
+    console.log("current score is" + currentScore);
+  };
+
+  const hold = function () {
+    dispatch({ type: "hold" });
+  };
+
+  return (
+    <main>
+      <section className="player">
+        <h2 className="name">Player 1</h2>
+        <p className="score">{scorePlayer1}</p>
+        <div className="current">
+          <p className="current-label">Current</p>
+          <p className="current-score">{currentScore}</p>
+        </div>
+      </section>
+      <section className="player">
+        <h2 className="name">Player 2</h2>
+        <p className="score">34</p>
+        <div className="current">
+          <p className="current-label">Current</p>
+          <p className="current-score">0</p>
+        </div>
+      </section>
+
+      <>
+        {dice ? (
+          <img
+            src={`/images/dice-${dice}.png`}
+            alt="Playing dice"
+            className="dice"
+          />
+        ) : null}
+        <button className="btn btn--new" onClick={reset}>
+          🔄 New game
+        </button>
+        <button className="btn btn--roll" onClick={rollDice}>
+          🎲 Roll dice
+        </button>
+        {dice ? (
+          <button className="btn btn--hold" onClick={hold}>
+            📥 Hold
+          </button>
+        ) : null}
+      </>
+    </main>
+  );
+}
+
+export default App;
